e8b0772202bd6815109ccd25b81db9288a2de7cc
Mateus Solar <mateusr.almeidas@gmail.com>
2026-02-11 20:41:10 +0800

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookstoreManagerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetBookById(int id)
+        {
+            return Ok();
+        }
+    }
+}
